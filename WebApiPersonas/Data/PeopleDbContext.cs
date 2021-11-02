79cecae64752b90cb8d80984608356fe6dff963f
Dvillano <danie.villano@gmail.com>
2021-11-02 09:30:02 -0300

    Add project files.

---
+++ WebApiPersonas/Data/PeopleDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using WebApiPersonas.Models;
+
+namespace WebApiPersonas.Data
+{
+    public class PeopleDbContext : DbContext
+    {
+        //Tene que estar el constructor con parametros en Core
+        public PeopleDbContext(DbContextOptions<PeopleDbContext> options) : base(options) { }
+
+        public DbSet<Person> People { get; set; }
+
+    }
+}
